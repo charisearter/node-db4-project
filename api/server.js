a199bb4e3eed05b0c8bf5599d1a2cd0c22f8118b
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-16 17:41:41 -0500

    trying to get seeds to work

--- api/server.js
+++ api/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const helmet = require("helmet");
 
 const recipeRouter = require('./recipes/recipes-router.js');
 
+const db = require("../data/connection.js");
+
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+
 server.use('/api/recipes', recipeRouter);
+
+server.get("/api/recipes", (req, res) => {
+  db("recipes")
+      .then(recipes => {
+          res.status(200).json({ data: recipes });
+      })
+      .catch(error => {
+          res.status(500).json({ 
+            messge: 'There was an error getting the recipes', 
+            error: error.message });
+      });
+});
+
 
 module.exports = server;
--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const knex = require("knex");
 
-const knexfile = require("../knexfile.js");
+const config = require("../knexfile.js");
 
-const knexConfig = knexfile.development;
+const db = config.development;
 
-module.exports = knex(knexConfig);
+module.exports = knex(db);
